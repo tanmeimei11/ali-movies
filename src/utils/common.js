5e4b6b68b1f3e59b489aa3a11fc4a3e600335059
xiangxiang <jiuyan@jiuyandeMacBook-Pro.local>
2018-01-18 11:47:28 +0800

    add common

---
+++ src/utils/common.js
@@ -0,0 +1,31 @@
+/**
+ * 格式化时间戳
+ * @param {*} date
+ * @param {*} isShowWeek
+ */
+const formatTime = (_date, isShowWeek) => {
+  var date = new Date(_date)
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  // const hour = date.getHours()
+  // const minute = date.getMinutes()
+  // const second = date.getSeconds()
+  // const week = weekdays[date.getDay()]
+  // if (isShowWeek) {
+  //   return `${formatNumber(month)}月${formatNumber(day)}日 ${week} ${[hour, minute].map(formatNumber).join(':')}`
+  // }
+  return `${year}.${formatNumber(month)}.${formatNumber(day)} `
+}
+/**
+ * 个位参数加0
+ * @param {*} n
+ */
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime
+}
