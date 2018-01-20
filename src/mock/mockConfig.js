d3da95eea87b6a08d4bc746952b47a7a8dc9b92a
小夫 <xiaofu@in66.com>
2018-01-20 18:59:57 +0800

    add

--- src/mock/mockConfig.js
+++ src/mock/mockConfig.js
@@ -2,12 +2,9 @@ import {DOMAIN, payUrl} from '@/utils/config';
 module.exports = {
   [`${DOMAIN}/mnp/order/create`]: 'orderCreate',
   [`${DOMAIN}/mnp/user/my`]: 'my',
-<<<<<<< HEAD
-  [`${DOMAIN}/mnp/order/result`]: 'result',
-=======
   [`${DOMAIN}/mnp/product/cfStatus`]: 'cfStatus',
   [`${DOMAIN}/info/cinemas`]: 'detail',
   [`${DOMAIN}/mnp/product/cfStatus`]: 'cfStatus',
->>>>>>> 9966e14f89a59ecf39e1767703525ffaf7ed95d5
+  [`${DOMAIN}/mnp/order/result`]: 'result',
   [`${payUrl}`]: 'signature'
 };
